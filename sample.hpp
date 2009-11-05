c2061939debceb8e47821725d613433b40acd91d
rubidium <rubidium@openttd.org>
2009-11-05 22:11:04 +0000

    (svn r17979) Fix: add GPL license where appropriate

--- sample.hpp
+++ sample.hpp
@@ -1,5 +1,23 @@
 /* $Id$ */
 
+/*
+ * catcodec is a tool to decode/encode the sample catalogue for OpenTTD.
+ * Copyright (C) 2009  Remko Bijker
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 /** @file sample.hpp Interface for Sample reading/writing */
 
 #ifndef SAMPLE_HPP
